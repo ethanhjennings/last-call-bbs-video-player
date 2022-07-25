5baacf515d33ecf62710c4a67bba5185e172d2ad
Ethan Jennings <ethanhjennings@gmail.com>
2022-07-24 21:07:11 -0700

    Rearranging things and adding comments

--- server_template.js
+++ server_template.js
@@ -15,31 +15,39 @@ function getName()
 
 function onConnect()
 {
+  drawText(filename, 17, 0, 19);
 }
 
 function onUpdate()
 {
+  // Draw in chunks of 28 pixels at a time so that they have time to display
   for (let i = 0; i < 28; i++) {
     drawText('█', frames[frame_idx][y][x], x, y);
     x++;
-    if (x >= 56) {
-      x = 0;
-      y += 1;
-      if (y >= 20) {
-        y = 0;
-        if (running) {
-        frame_idx += 1;
-          if (frame_idx > frames.length) {
-            frame_idx = 0; // loop video back around
-          }
+  }
+
+  // Advance rows and frames as needed
+  if (x >= 56) {
+    x = 0;
+    y += 1;
+    if (y >= 20) {
+      y = 0;
+      if (running) {
+      frame_idx += 1;
+        if (frame_idx > frames.length) {
+          // Loop video at the end
+          frame_idx = 0;
         }
       }
     }
   }
+
   if (!running) {
+    // Pause button
     fillArea('█', 17, 0, 0, 1, 3);
     fillArea('█', 17, 2, 0, 1, 3);
   }
+
   drawText(filename, 17, 0, 19);
 }
 
@@ -49,7 +57,7 @@ function onInput(key)
     // Pause the video
     running = !running;
   }
-  if (key === 20 /* right */ || (key === 32 && running)) {
+  if (key === 20 /* right */ || (key === 32 && running) /* video resumed */) {
     // Advance a frame
     frame_idx = (frame_idx+1)%frames.length;
     y = 0;
